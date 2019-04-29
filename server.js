0ff6008f23d876b5a24bc755e23b8cd9f5c94022
tapperm <maxtorres@seattleacademy.org>
2019-04-29 08:51:31 -0700

    Add node_modules

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-console.log('helloworld');
+// console.log('helloworld');
 
+var gsjson = require('google-spreadsheet-to-json');
 
-
+gsjson({
+    spreadsheetId: '16X19oHUdt6NPkytGrM6VMHqxBE8g6BfEvztNIv289Dk',
+    // other options...
+})
+.then(function(result) {
+    console.log(result.length);
+    console.log(result);
+})
+.catch(function(err) {
+    console.log(err.message);
+    console.log(err.stack);
+});

379801ccb3aa7ffad74b9512dd142691d5f176f9
tapperm <maxtorres@seattleacademy.org>
2019-04-29 09:06:35 -0700

    add express server for google sheet sharing

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 // console.log('helloworld');
 
+
+const express = require('express')
+const app = express()
+const port = 1214
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
 var gsjson = require('google-spreadsheet-to-json');
 
 gsjson({
-    spreadsheetId: '16X19oHUdt6NPkytGrM6VMHqxBE8g6BfEvztNIv289Dk',
-    // other options...
+        spreadsheetId: '16X19oHUdt6NPkytGrM6VMHqxBE8g6BfEvztNIv289Dk',
+        // other options...
+    })
+    .then(function(result) {
+        console.log(result.length);
+        console.log(result);
+    })
+    .catch(function(err) {
+        console.log(err.message);
+        console.log(err.stack);
+    });
+
+app.get('/json', (req, res) => {
+    
+    gsjson({
+        spreadsheetId: '16X19oHUdt6NPkytGrM6VMHqxBE8g6BfEvztNIv289Dk',
+        // other options...
+    })
+    .then(function(result) {
+        console.log(result.length);
+        console.log(result);
+        res.send(result)
+    })
+    .catch(function(err) {
+        console.log(err.message);
+        console.log(err.stack);
+    });
+
 })
-.then(function(result) {
-    console.log(result.length);
-    console.log(result);
-})
-.catch(function(err) {
-    console.log(err.message);
-    console.log(err.stack);
-});
